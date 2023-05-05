a629cb1b1989251b7f63bcd3436a1c55cc1c87ab
Daniel-pulga <121583727+Daniel-pulga@users.noreply.github.com>
2023-05-05 14:06:20 -0500

    Add files via upload

---
+++ 01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(12 + 19 + 32 - 10);
+
+console.log("Daniel");
+console.log(34);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let daniel_matilda = "JM";
+let $function = 27;
+
+let person = "daniel";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
